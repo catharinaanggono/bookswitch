e4a46718a98e43adde5d09ac6fe6e78ac8bdf1ee
veronicateng13 <55087645+veronicateng13@users.noreply.github.com>
2020-10-06 17:27:39 +0800

    js/

---
+++ bookswitch.js
@@ -0,0 +1,25 @@
+// 
+function (all_search) {
+
+}
+
+// 
+function (genre_search) {
+
+}
+
+// 
+function (display_book_details) {
+
+}
+
+// page 7
+function (my_books) {
+
+}
+
+// page 5
+function (reviews) {
+
+}
+
