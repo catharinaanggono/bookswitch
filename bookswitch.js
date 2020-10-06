eb8afaf058f32325dcd9ae406f8dccdf97f0140d
jesst79 <jtedja.2019@smu.edu.sg>
2020-10-06 18:54:05 +0800

    fix

--- bookswitch.js
+++ bookswitch.js
@@ -22,30 +22,3 @@ function (my_books) {
 function (reviews) {
 
 }
-
-// 
-function (all_search) {
-    //git commit test
-    console.log("Git Commit Test");
-
-}
-
-// 
-function (genre_search) {
-
-}
-
-// 
-function (display_book_details) {
-
-}
-
-// page 7
-function (my_books) {
-
-}
-
-// page 5
-function (reviews) {
-
-}
