cc42d9e522934f3aac4822205e7473afd97ce7f0
trishachua2019 <72441209+trishachua2019@users.noreply.github.com>
2020-11-10 22:12:38 +0800

    Update listingDAO.php
    added getListing

--- model/listingDAO.php
+++ model/listingDAO.php
@@ -5,20 +5,40 @@
 
     class listingDAO { 
 
-        public function addCopy($userid, $isbn) { 
+        public function addCopy($userid, $isbn, $status) { 
 
             $conn = new ConnectionManager(); 
             $pdo = $conn->getConnection(); 
-            $sql = "INSERT INTO listings (userID,isbn) 
-            VALUES (:userID, :isbn)";
+            $sql = "INSERT INTO listings (userID,isbn,status) 
+            VALUES (:userID, :isbn, :status)";
             $stmt = $pdo->prepare($sql); 
             $stmt->bindParam(':userID', $userid); 
             $stmt->bindParam(':isbn', $isbn); 
-            $status = $stmt->execute(); 
+            $stmt->bindParam(':status', $status);
+            $return = $stmt->execute(); 
             $stmt->closeCursor(); 
             $pdo = null; 
-            return $status;
+            return $return;
 
+        }
+
+        public function getListing($userid) {
+            $conn = new ConnectionManager();
+            $pdo = $conn->getConnection();
+            $sql = 'SELECT * FROM listings WHERE userid=:userid';
+            $stmt = $pdo->prepare($sql);
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $stmt->bindParam(':userid', $userid, PDO::PARAM_STR);
+            
+            $stmt->execute();
+            $result = null;
+            $listingsRecords = []; 
+            while ($row = $stmt->fetch()) {
+                $listingsRecords[] = [$row['isbn'], $row['status']];
+            }
+            $stmt->closeCursor();
+            $pdo = null;
+            return $listingsRecords;
         }
 
 
