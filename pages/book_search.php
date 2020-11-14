5ec9a18a5671fedfb99c460d10040fc47058615d
Catharina ANGGONO <catharinaa.2019@sis.smu.edu.sg>
2020-11-14 14:00:06 +0800

    zzz

--- pages/book_search.php
+++ pages/book_search.php
@@ -61,7 +61,7 @@
                 
                 <div class="collapse navbar-collapse order-3 order-lg-2" id="navbarResponsive">
                     <ul class="navbar-nav text-uppercase ml-auto">
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger active" href="../pages/book_genre.html">Genre</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="../pages/book_genre.html">Genre</a></li>
                         <li class="nav-item">
                           <div class="search" id="search">
                             <input id="autocomplete" type="text" placeholder="Search Title, Author, ISBN">
