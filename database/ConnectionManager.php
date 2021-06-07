76f6466e0cb2a80f7114175506b700604c893b4f
catharinaanggono <catharinaa.2019@sis.smu.edu.sg>
2021-06-07 19:06:23 +0800

    Update ConnectionManager.php

--- database/ConnectionManager.php
+++ database/ConnectionManager.php
@@ -24,9 +24,9 @@
         $pdoObject = new PDO("mysql:host=$servername;dbname=$dbname;port=$port", $username, $password);
         // set the PDO error mode to exception
         $pdoObject->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected successfully";
+        // echo "Connected successfully";
       } catch(PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
+        // echo "Connection failed: " . $e->getMessage();
       }
 
       return $pdoObject;
