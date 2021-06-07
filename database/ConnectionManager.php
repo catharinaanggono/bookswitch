d5f3f19a86790c09962e62dc544d47dc4c3e4294
catharinaanggono <catharinaa.2019@sis.smu.edu.sg>
2021-06-07 14:58:15 +0800

    Update ConnectionManager.php

--- database/ConnectionManager.php
+++ database/ConnectionManager.php
@@ -4,16 +4,30 @@
   class ConnectionManager {
 
     public function getConnection() {
+      // $servername = 'us-cdbr-east-04.cleardb.com';
+      // $dbname = 'heroku_45f81d94bed0ef2';
+      // $username = 'bfdda4ee641b04';
+      // $password = 'b004a5a6';
+      // $port = 3306;
+      // $url  = "mysql:host=$servername;dbname=$dbname;port=$port";
+
+      // $pdoObject = new PDO($url, $username, $password);
+      // $pdoObject->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      
       $servername = 'us-cdbr-east-04.cleardb.com';
-      $dbname = 'heroku_45f81d94bed0ef2';
       $username = 'bfdda4ee641b04';
       $password = 'b004a5a6';
+      $dbname = 'heroku_45f81d94bed0ef2';
       $port = 3306;
-      $url  = "mysql:host=$servername;dbname=$dbname;port=$port";
 
-      $pdoObject = new PDO($url, $username, $password);
-      $pdoObject->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      
+      try {
+        $pdoObject = new PDO("mysql:host=$servername;dbname=$dbname;port=$port", $username, $password);
+        // set the PDO error mode to exception
+        $pdoObject->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
+      } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+      }
 
       return $pdoObject;
 
