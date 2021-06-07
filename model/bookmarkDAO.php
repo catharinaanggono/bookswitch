a1d5865726c9e4ba0f7424ea88798bacea7d00d7
catharinaanggono <catharinaa.2019@sis.smu.edu.sg>
2021-06-07 18:36:58 +0800

    TRY PLS

--- model/bookmarkDAO.php
+++ model/bookmarkDAO.php
@@ -1,7 +1,7 @@
 <?php 
 
     ### Connecting to the file which consists codes to connect to the database (Essential for PDOStatements)
-    require_once "/app/model/common.php";
+    require_once $_SERVER['DOCUMENT_ROOT']."/app/model/common.php";
 
     class bookmarkDAO { 
 
--- model/common.php
+++ model/common.php
@@ -3,12 +3,12 @@
     ### Each time when an object is created, it direct to the file with the codes. 
     spl_autoload_register (
         function ($class) {
-            require_once "/app/model/$class.php";
+            require_once $_SERVER['DOCUMENT_ROOT']."/app/model/$class.php";
         }
     );
 
     ### Connect to the database 
-    require_once "/app/database/ConnectionManager.php"; 
+    require_once $_SERVER['DOCUMENT_ROOT']."/app/database/ConnectionManager.php"; 
 
     ### Starting/commencing the session 
     session_start();
