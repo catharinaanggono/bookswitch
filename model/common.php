55d075454f4793ea12807b21772eb2c1b1d66849
catharinaanggono <catharinaa.2019@sis.smu.edu.sg>
2021-06-07 18:40:11 +0800

    pl

--- model/common.php
+++ model/common.php
@@ -3,12 +3,12 @@
     ### Each time when an object is created, it direct to the file with the codes. 
     spl_autoload_register (
         function ($class) {
-            require_once $_SERVER['DOCUMENT_ROOT']."/app/model/$class.php";
+            require_once $_SERVER['DOCUMENT_ROOT']."/model/$class.php";
         }
     );
 
     ### Connect to the database 
-    require_once $_SERVER['DOCUMENT_ROOT']."/app/database/ConnectionManager.php"; 
+    require_once $_SERVER['DOCUMENT_ROOT']."/database/ConnectionManager.php"; 
 
     ### Starting/commencing the session 
     session_start();
