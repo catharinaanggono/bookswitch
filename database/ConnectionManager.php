0328df87de59581bf1dd260b9d20fb8cb9f2aa88
catharinaanggono <catharinaa.2019@sis.smu.edu.sg>
2021-12-08 14:36:44 +0800

    Revert "Update ConnectionManager.php"
    This reverts commit b80ac1ecf6c6e46a06426eb0e55eca8da1983195.

--- database/ConnectionManager.php
+++ database/ConnectionManager.php
@@ -4,41 +4,32 @@
   class ConnectionManager {
 
     public function getConnection() {
-    //   // $servername = 'us-cdbr-east-04.cleardb.com';
-    //   // $dbname = 'heroku_45f81d94bed0ef2';
-    //   // $username = 'bfdda4ee641b04';
-    //   // $password = 'b004a5a6';
-    //   // $port = 3306;
-    //   // $url  = "mysql:host=$servername;dbname=$dbname;port=$port";
+      // $servername = 'us-cdbr-east-04.cleardb.com';
+      // $dbname = 'heroku_45f81d94bed0ef2';
+      // $username = 'bfdda4ee641b04';
+      // $password = 'b004a5a6';
+      // $port = 3306;
+      // $url  = "mysql:host=$servername;dbname=$dbname;port=$port";
 
-    //   // $pdoObject = new PDO($url, $username, $password);
-    //   // $pdoObject->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      // $pdoObject = new PDO($url, $username, $password);
+      // $pdoObject->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       
-    //   // $servername = 'us-cdbr-east-04.cleardb.com';
-    //   // $username = 'bfdda4ee641b04';
-    //   // $password = 'b004a5a6';
-    //   // $dbname = 'heroku_45f81d94bed0ef2';
-    //   // $port = 3306;
+      $servername = 'us-cdbr-east-04.cleardb.com';
+      $username = 'bfdda4ee641b04';
+      $password = 'b004a5a6';
+      $dbname = 'heroku_45f81d94bed0ef2';
+      $port = 3306;
 
-    //   try {
-    //     $pdoObject = new PDO("mysql:host=$servername;dbname=$dbname;port=$port", $username, $password);
-    //     // set the PDO error mode to exception
-    //     $pdoObject->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //     // echo "Connected successfully";
-    //   } catch(PDOException $e) {
-    //     // echo "Connection failed: " . $e->getMessage();
-    //   }
+      try {
+        $pdoObject = new PDO("mysql:host=$servername;dbname=$dbname;port=$port", $username, $password);
+        // set the PDO error mode to exception
+        $pdoObject->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // echo "Connected successfully";
+      } catch(PDOException $e) {
+        // echo "Connection failed: " . $e->getMessage();
+      }
 
-    //   return $pdoObject;
-
-    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-    $server = $url["host"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $db = substr($url["path"], 1);
-  
-    $conn = new mysqli($server, $username, $password, $db);
+      return $pdoObject;
 
     }
 }
